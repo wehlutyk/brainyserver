5fbf4809082b87c0c780f45edd99cf0adbd7d935
Sébastien Lerique <seblerique@wanadoo.fr>
2012-06-27 06:52:51 +0200

    Added some validation for registration

--- brainyserver/forms.py
+++ brainyserver/forms.py
@@ -6,9 +6,33 @@
 
 from flask.ext.wtf import Form, PasswordField, TextField, validators
 
+from brainyserver.mongodb import Researcher
+
+
+class UsernameMongoValidator(object):
+    
+    def __call__(self, form, field):
+        if Researcher.objects(username=field.data).count() != 0:
+            raise validators.ValidationError(('That username is already '
+                                              'in use'))
+
+
+class EmailMongoValidator(object):
+    
+    def __call__(self, form, field):
+        if Researcher.objects(email=field.data).count() != 0:
+            raise validators.ValidationError(('That email address is already '
+                                              'in use'))
+
 
 class RegisterForm(Form):
-    username = TextField('Username', [validators.Length(min=3, max=50)])
+    username = TextField('Username', [validators.Length(min=3, max=50),
+                                      UsernameMongoValidator()])
     email = TextField('Email Address', [validators.Length(min=3, max=50),
-                                        validators.Email()])
-    password = PasswordField('Password')
+                                        validators.Email(),
+                                        EmailMongoValidator()])
+    password = PasswordField('Password', [validators.Length(min=8,
+                        message='Password must be at least 8 characters long')])
+    confirmpassword = PasswordField('Confirm password',
+                                    [validators.EqualTo('password',
+                                                    "Passwords don't match")])
